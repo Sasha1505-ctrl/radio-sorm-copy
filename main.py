7b675b3ed0863f30b6876b6d709fc9f1692ba583
bodosovaa <bodosovaa@srv-bourbon-nvr.dev.local>
2019-11-28 18:40:01 +0000

    Bulk insert Reg data

--- main.py
+++ main.py
@@ -23,22 +23,43 @@ regs = Table('regs', metadata,
         Column('prev_location', Integer),
         )
 metadata.create_all(engine)
+
 conn = engine.connect()
-
+buffer = []
 for blk in target.block:
-    for event in blk.events.event:
-        if event.body.type == Tetra.Types.reg:
-            served_nitsi = "".join([hex(i)[2:] for i in event.body.served_nitsi])
-            ins = regs.insert().values(served_nitsi=served_nitsi,
-                    location=event.body.location,
-                    prev_location=event.body.prev_location)
-            result = conn.execute(ins)
+    conn.execute(
+        regs.insert(),
+        [
+            dict(
+                served_nitsi="".join([hex(i)[2:] for i in event.body.served_nitsi]),
+                location=event.body.location,
+                prev_location=event.body.prev_location,
+            )
+            for event in blk.events.event if event.body.type == Tetra.Types.reg
+        ],
+    )
+#    for event in blk.events.event:
+#        co
+#        if event.body.type == Tetra.Types.reg:
+#            served_nitsi = "".join([hex(i)[2:] for i in event.body.served_nitsi])
+#            buffer.append({
+#                served_nitsi:served_nitsi,
+#                location:event.body.location,
+#                prev_location:event.body.prev_location
+#            })
+#    session.bulk_insert_mapping(buffer)
+#    buffer = []
+#            ins = regs.insert().values(served_nitsi=served_nitsi,
+#                    location=event.body.location,
+#                    prev_location=event.body.prev_location)
+#            result = conn.execute(ins)
 
-            date_obj = event.body.timestamp
-            print(datetime(date_obj.full_year, date_obj.month.as_int,
-                           date_obj.day.as_int, date_obj.hour.as_int,
-                           date_obj.min.as_int, date_obj.sec.as_int,
-                           date_obj.msec.as_int))
-            print(event.body.location, event.body.prev_location)
+#            date_obj = event.body.timestamp
+#            print(datetime(date_obj.full_year, date_obj.month.as_int,
+#                           date_obj.day.as_int, date_obj.hour.as_int,
+#                           date_obj.min.as_int, date_obj.sec.as_int,
+#                           date_obj.msec.as_int))
+#            print(event.body.location, event.body.prev_location)
             #print("".join([f"{(ord(x)>>4)*10+(ord(x)&0x0F):02}" for x in event.body.served_nitsi]))
+
 
