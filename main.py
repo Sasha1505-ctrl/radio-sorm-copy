6d289f2d24807c2a9da4d8ff3d29c09354b15990
bDrwx <a.bodosov@gmail.com>
2019-12-02 23:13:24 +0300

    fix for pep8

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
 from kaitai.parser.tetra import Tetra
-from datetime import datetime, date, time
+from datetime import datetime
 from pprint import pprint
 import click
+
 
 def bcdDigits(chars):
     for char in chars:
@@ -11,23 +13,24 @@ def bcdDigits(chars):
                 return
             yield val
 
+
 @click.command()
 @click.argument('filename', type=click.Path(exists=True))
 def parseCDR(filename):
 
     from sqlalchemy import create_engine
-    engine = create_engine('sqlite:///test.db',echo=True)
+    engine = create_engine('sqlite:///test.db', echo=True)
 
-    from sqlalchemy import Table, Column, Integer, String,DateTime, MetaData,PrimaryKeyConstraint
+    from sqlalchemy import Table, Column, Integer, String, DateTime, MetaData,    PrimaryKeyConstraint
     metadata = MetaData()
-    regs = Table('regs', metadata,
+    regs = Table(
+            'regs', metadata,
             Column('id', Integer, primary_key=True),
             Column('served_nitsi', String(12)),
             Column('location', Integer),
             Column('prev_location', Integer),
             Column('reg_at', DateTime),
-            PrimaryKeyConstraint('id', 'served_nitsi',
-                name='reg_pk')
+            PrimaryKeyConstraint('id', 'served_nitsi', name='reg_pk')
             )
     metadata.create_all(engine)
 
@@ -35,26 +38,26 @@ def parseCDR(filename):
 
     conn = engine.connect()
     for blk in target.block:
-    #   conn.execute(
-    #       regs.insert(),
-    #       [
-    #           dict(
-    #               id=event.body.seq_num,
-    #               served_nitsi="".join([hex(i)[2:] for i in event.body.served_nitsi]),
-    #               location=event.body.location,
-    #               prev_location=event.body.prev_location,
-    #               reg_at=datetime(
-    #                   event.body.timestamp.full_year,
-    #                   event.body.timestamp.month.as_int,
-    #                   event.body.timestamp.day.as_int,
-    #                   event.body.timestamp.hour.as_int,
-    #                   event.body.timestamp.min.as_int,
-    #                   event.body.timestamp.sec.as_int,
-    #                   event.body.timestamp.msec.as_int),
-    #               )
-    #           for event in blk.events.event if event.body.type == Tetra.Types.reg
-    #       ]
-    #   )
+        # conn.execute(
+        #     regs.insert(),
+        #     [
+        #         dict(
+        #             id=event.body.seq_num,
+        #             served_nitsi="".join([hex(i)[2:] for i in event.body.served_nitsi]),
+        #             location=event.body.location,
+        #             prev_location=event.body.prev_location,
+        #             reg_at=datetime(
+        #                 event.body.timestamp.full_year,
+        #                 event.body.timestamp.month.as_int,
+        #                 event.body.timestamp.day.as_int,
+        #                 event.body.timestamp.hour.as_int,
+        #                 event.body.timestamp.min.as_int,
+        #                 event.body.timestamp.sec.as_int,
+        #                 event.body.timestamp.msec.as_int),
+        #             )
+        #         for event in blk.events.event if event.body.type == Tetra.Types.reg
+        #     ]
+        # )
         buffer = []
         call_reference = None
         for event in blk.events.event:
@@ -100,12 +103,12 @@ def parseCDR(filename):
                 else:
                     # Продолжаем обрабатывать звонок
                     call_reference = event.body.call_reference
-    
+
             if event.body.type == Tetra.Types.reg:
                 """ Обработка записи о регистрации абонента """
                 served_nitsi = "".join([hex(i)[2:] for i in event.body.served_nitsi])
                 vdate_obj = event.body.timestamp
-                
+
                 buffer.append(
                     dict(
                         id = event.body.seq_num,
@@ -125,5 +128,7 @@ def parseCDR(filename):
                 )
         conn.execute(regs.insert(), buffer)
         buffer = []
+
+
 if __name__ == '__main__':
     parseCDR()
