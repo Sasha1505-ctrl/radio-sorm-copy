2fde12450a67f45a43f52560d0fbd371f8ba2a09
bDrwx <a.bodosov@gmail.com>
2020-01-12 12:18:35 +0300

    Hz

--- cdr.py
+++ cdr.py
@@ -10,17 +10,19 @@ class Cdr:
     def add_toc(self, toc, group=False, termination='ok'):
         if self.__call_reference != toc.call_reference:
             raise ValueError("uндикаторы звонка не совпадают self.{__call_reference} != {call_reference}".format(self.__call_reference, toc.call_reference))
-        if toc.members = 65535:
+        if toc.members == 65535:
             # Персональный вызов
             if self.__call_reference == 0:
                 # Вызов не состоялся, ошибка соединения.
+                print(f'add_toc() - abonent is busy {toc.termination}')
+            else:
+                # Обработка персонального вызова. Ожидаем получения ТСС или 
+                print("add_toc() - Start call parser")
         else:
             # Групповой вызов
+            print("add_toc() - Group Call")
         # Init head rec
         self.seq_num = toc.seq_num
-        print("add_toc() - calling")
-        if termination != 'ok':
-            print("add_toc() - rejected call")
     
     def add_tcc(self, tcc):
         if self.__call_reference != tcc.call_reference:
