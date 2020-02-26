d6aedc30ab6d3e4107cf5c2fc0216f2724a13c72
bodosovaa <bodosovaa@stn.transneft.ru>
2020-02-26 21:37:23 +0300

    Fix test

--- tests/test_first.py
+++ tests/test_first.py
@@ -1,12 +1,14 @@
 from sorm.utility import * 
+from datetime import timedelta
+
 import pytest
 
 def test_dec_to_sec():
-    assert 1 == to_sec(100)
+    assert timedelta(seconds=1) == to_sec(100)
 
-@pytest.mark.xfail(raises=TypeError)
 def test_str_to_sec():
-    assert 1 == to_sec("hellp")
+    with pytest.raises(TypeError):
+        to_sec("hellp")
 
 def test_bcd_to_str_con():
     src = b'\x10\x20\x25\x00\x00\x75\x78\x00\x46\xff'
